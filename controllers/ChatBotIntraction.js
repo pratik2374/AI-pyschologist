d55fb57d4e5d194196f2061176bb25fbf901327b
Sourabh Gupta <123104060@nitkkr.ac.in>
2026-03-11 23:45:42 +0530

    chat work done

--- controllers/ChatBotIntraction.js
+++ controllers/ChatBotIntraction.js
@@ -1,13 +1,62 @@
 
 const User = require('../models/User');
-
+const Chat = require('../models/Chat');
+require("dotenv").config();
 exports.pastHistory = async(req,res)=>{
     try{
         const user_id = req.decoded?.userid;
-        
-        
+        const response = await Chat.find({user_id});
+
+        res.status(200).json({
+            success:true,
+            message:"response recieved of past chats",
+            response
+        })
+
     }
     catch(err){
+        res.status(500).json({
+            success: false,
+            message:"Error occured while fetching past chat",
+            err
+        })
+    }
+}
 
+
+//current logs
+
+exports.currChat = async(req,res)=>{
+    try{      
+        const {user_id,query} = req.body;
+        const api = process.env.currentChatdjangoApi;
+
+        const response = await fetch(api, {
+          method: "POST",
+          headers: {
+           "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            user_id,   
+            query 
+          })
+        });
+        const data = await response.json();
+
+        return res.status(200).json({
+            success:true,
+            message:"curr chat api working good",
+            data
+        })
+
+
+
+    }
+    catch(err){
+        res.status(500).json({
+            success: false,
+            message:"Error occured while current chat",
+            err
+        })
     }
 }
--- routes/route.js
+++ routes/route.js
@@ -2,12 +2,14 @@ const express = require("express");
 const route = express.Router();
 
 const {AuthN, isVisitor, isAdmin} = require("../middlewares/auth");
-
+const {pastHistory,currChat} = require("../controllers/ChatBotIntraction")
 //Auth
 const {generateOTP, signup, login} = require("../controllers/Auth");
 route.post("/generateotp", generateOTP);
 route.post("/signup", signup);
 route.post("/login", login);
+route.get("/pastChat/:id",pastHistory);
+route.post("/currChat",currChat);
 
 
 module.exports = route;
